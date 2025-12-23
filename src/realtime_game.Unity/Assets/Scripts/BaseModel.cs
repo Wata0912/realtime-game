0a2cc0ddcf27d000701f3127a82a506ad8a56159
S-JOBI24-079\student <241211@st.yoshida-g.ac.jp>
2025-12-23 16:13:49 +0900

    β

--- src/realtime_game.Unity/Assets/Scripts/BaseModel.cs
+++ src/realtime_game.Unity/Assets/Scripts/BaseModel.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public class BaseModel : MonoBehaviour
 {
-    protected const string ServerURL = "http://10.70.41.79:5244";
+    protected const string ServerURL = "http://localhost:5244";
 }
