6302f0a1d919497b4a9f09404b43d172fa6d9348
S-JOBI24-079\student <241211@st.yoshida-g.ac.jp>
2025-12-24 09:29:05 +0900

    ベイのステータス調整

--- src/realtime_game.Unity/Assets/Scripts/BaseModel.cs
+++ src/realtime_game.Unity/Assets/Scripts/BaseModel.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public class BaseModel : MonoBehaviour
 {
-    protected const string ServerURL = "http://localhost:5244";
+    //protected const string ServerURL = "http://localhost:5244";
+    protected const string ServerURL = "http://10.70.41.68:5244/";
 }
