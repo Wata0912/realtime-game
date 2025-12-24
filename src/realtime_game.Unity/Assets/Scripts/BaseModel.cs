ca808160dfd2c9d31868a5425f730c0b4427259e
S-JOBI24-079\student <241211@st.yoshida-g.ac.jp>
2025-12-24 10:16:11 +0900

    ベイの生成カーソルの修正

--- src/realtime_game.Unity/Assets/Scripts/BaseModel.cs
+++ src/realtime_game.Unity/Assets/Scripts/BaseModel.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 public class BaseModel : MonoBehaviour
 {
-    //protected const string ServerURL = "http://localhost:5244";
-    protected const string ServerURL = "http://10.70.41.68:5244/";
+    //protected const string ServerURL = "http://localhost:5244";       //ローカル接続用
+    protected const string ServerURL = "http://10.70.41.68:5244/";      //平尾接続用
 }
